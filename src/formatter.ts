7bf46b55cde30bf7f8ae1e7e50d23870aaf3b69d
Hideo Matsumoto <hideo-m@pekeq.com>
2022-11-24 21:06:55 +0900

    add minify

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,13 +1,15 @@
 import { Command } from "commander";
 import * as fs from "fs/promises";
 import _ from "lodash";
+import { minify } from "html-minifier";
 
 async function main() {
   const program = new Command();
   program
     .option("-i, --input <file>", "Input filename", "data/aws-services.json")
     .option("-o, --output <file>", "Output directory", "public/index.html")
-    .option("-t, --template <file>", "HTML template file", "src/template.html");
+    .option("-t, --template <file>", "HTML template file", "src/template.html")
+    .option("-m, --minify", "Minify HTML");
   program.parse();
   const options = program.opts();
 
@@ -16,7 +18,17 @@ async function main() {
 
   const template = await fs.readFile(options.template);
   const compiled = _.template(template.toString());
-  const html = compiled({ services });
+  let html = compiled({ services });
+
+  if (options.minify) {
+    html = minify(html, {
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyCSS: true,
+      minifyJS: true,
+    });
+  }
+
   await fs.writeFile(options.output, html);
 }
 
